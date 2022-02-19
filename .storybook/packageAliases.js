e5ace571b6c8522983b9ab6d84ddd84009baf17e
Konrad Fedorczyk <contact@realhe.ro>
2022-02-19 19:57:44 +0100

    Update package aliases

--- .storybook/packageAliases.js
+++ .storybook/packageAliases.js
@@ -3,8 +3,14 @@ const path = require("path");
 const PACKAGES_PATH = "../web-ui/packages/";
 
 module.exports = {
+  // @retrolove-games/ui-themes
   "@retrolove-games/ui-themes": path.resolve(
     __dirname,
     `${PACKAGES_PATH}/themes/src/index`
+  ),
+  // @retrolove-games/ui-hello-world
+  "@retrolove-games/ui-hello-world": path.resolve(
+    __dirname,
+    `${PACKAGES_PATH}/hello-world/src/index.ts`
   ),
 };
