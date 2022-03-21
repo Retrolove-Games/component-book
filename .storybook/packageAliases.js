833801fb2886cb14b7b5c6969c2c2903cdacf948
Konrad Fedorczyk <fedek6@gmail.com>
2022-03-21 20:43:16 +0100

    Refactor package aliases

--- .storybook/packageAliases.js
+++ .storybook/packageAliases.js
@@ -2,15 +2,17 @@ const path = require("path");
 
 const PACKAGES_PATH = "../web-ui/packages/";
 
-module.exports = {
-  // @retrolove-games/ui-themes
-  "@retrolove-games/ui-themes": path.resolve(
+const aliases = {};
+
+[
+  "themes",
+  "hello-world",
+  "logo"
+].map(alias => {
+  aliases[`@retrolove-games/ui-${alias}`] = path.resolve(
     __dirname,
-    `${PACKAGES_PATH}/themes/src/`
-  ),
-  // @retrolove-games/ui-hello-world
-  "@retrolove-games/ui-hello-world": path.resolve(
-    __dirname,
-    `${PACKAGES_PATH}/hello-world/src/`
-  ),
-};
+    `${PACKAGES_PATH}/${alias}/src/`
+  )
+});
+
+module.exports = aliases;
