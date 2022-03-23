6eb364155d50c42cba7252ec6412c638770b6f7d
Konrad Fedorczyk <fedek6@gmail.com>
2022-03-23 11:50:53 +0100

    Update configuration

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,6 +2,10 @@ import React from "react";
 import { useDarkMode } from "storybook-dark-mode";
 import { darkTheme } from "@retrolove-games/ui-themes";
 import { DocsContainer } from './DocsContainer';
+const { addDecorator } = require('@storybook/react');
+const { withPropsTable } = require('storybook-addon-react-docgen');
+
+addDecorator(withPropsTable);
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
